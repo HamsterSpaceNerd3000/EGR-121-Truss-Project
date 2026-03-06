a9d003ad8bf47d141347dddac60817f2779571d0
SpaceNerd0717 <forjne@gmail.com>
2026-03-06 10:46:35 -0500

    Single Member Arduino Code Complete!

--- Truss-Main/Truss-Main.ino
+++ Truss-Main/Truss-Main.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+// For now this is only for one member.
+#include <Adafruit_NeoPixel.h>
 
+// Variable Init
+const int sensorPin = A0;
+int sensorVal = 0;
+float Vin = 5;
+float Vout = 0;
+// Reference resistor measurement
+float Rref = 990;
+float R = 0;
+
+// LED pin setup
+const int ledPin = 3;
+const int pixelCount = 16;
+
+// Led setup
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelCount, ledPin, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorVal = analogRead(sensorPin);
+  Vout = (Vin*sensorVal)/1023;
+  R - Rref * (1/ ((Vin - Vout) -1));
+  Serial.println(R);
 
+  // If / Then statement to handle LED display
+  // Control
+  int controlMin = 320;
+  int controlMax = 340;
+  if (controlMin <= R <= controlMax) {
+    for (int i = 0; i < pixelCount; i++) {
+      strip.setPixelColor(i, strip.Color(0, 255, 0));
+    }
+  } else if (R > controlMax) {
+    for (int i = 0; i < pixelCount; i++) {
+      strip.setPixelColor(i, strip.Color(255, 0, 0));
+    }
+  } else if (R < controlMin) {
+    for (int i = 0; i < pixelCount; i++) {
+      strip.setPixelColor(i, strip.Color(0, 0, 255));
+    }
+  } else {
+    for (int i = 0; i < pixelCount; i++) {
+      strip.setPixelColor(i, strip.Color(255, 255, 255));
+    }
+  }
+  
 }
